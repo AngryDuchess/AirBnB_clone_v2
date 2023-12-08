d1598c93801532f1c33e557ad93545e6fb0ee707
Mahama Hamida <mahamahamida@gmail.com>
2023-12-08 00:58:19 +0000

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" a function"""
+""" """
 
 from fabric.api import local, run, env, put
 from datetime import datetime as time
 from os import path
 
 
-env.hosts = ["18.207.233.37"]
+env.hosts = ["ubuntu@154.161.255.250", "ubuntu@54.236.33.119"]
 
 
 def do_pack():
-    """ do pack function """
+    """ """
     try:
         time_stamp = time.now().strftime("%Y%m%d%H%M%S")
         saved_path = "versions/web_static_{}.tgz".format(
@@ -27,28 +27,33 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """ do deploy function"""
+    """ """
     try:
         if not path.exists(archive_path):
             return False
 
-        releases = "/data/web_static/releases/"
+        releases = "/data/web_static/releases"
         web_static_dir = path.basename(archive_path).split(".")[0]
 
         put(local_path=archive_path, remote_path="/tmp/")
+
+        run("rm -rf {}/{}".format(
+            releases,
+            web_static_dir
+        ))
 
         run("mkdir -p {}/{}".format(
             releases,
             web_static_dir
         ))
-        run("tar -xzf /tmp/{} -C {}/{}".format(
-            archive_path,
+        run("tar -xzf /tmp/{}.tgz -C {}/{}".format(
+            web_static_dir,
             releases,
             web_static_dir
         ))
 
-        run("rm -f /tmp/{}".format(
-            archive_path
+        run("rm -f /tmp/{}.tgz".format(
+            web_static_dir
         ))
 
         run("mv {0}/{1}/web_static/* {0}/{1}".format(
