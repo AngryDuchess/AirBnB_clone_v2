3034fe331c850da1877b2b6ecf9acee0138f4cd0
Mahama Hamida <mahamahamida@gmail.com>
2023-12-24 12:57:24 +0000

    Add Flask route for displaying "C" followed by text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" starts a flask application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """hello function"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def holberton():
+    """displays hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display(text):
+    """"
+    display “C ” followed by
+    the value of the text variable
+    """
+    if "_" in text:
+        text = text.replace("_", " ")
+    return "C {}".format(text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
