5ce7f66796aa349550afeaf34d81b133620e7d09
Mahama Hamida <mahamahamida@gmail.com>
2023-12-25 15:39:04 +0000

    Delete main.delete.py and update states_list.py and 7-states_list.html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
 """ starts a flask application"""
 from flask import Flask, render_template
+from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def close_db(exception):
+    """closes connection"""
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
-def number_odd_even(n):
+def states():
     """renders an html odd or even template"""
-    return render_template('6-number_odd_or_even.html', n=n)
+    list_states = storage.all(State).values()
+    return render_template('7-states_list.html', states=list_states)
 
 
 if __name__ == '__main__':
